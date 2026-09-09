a34270840fb562a3f680ded4a349036e8c7a9269
ChristianM <anguselmasfeo123@gmail.com>
2026-09-09 14:02:26 -0600

    Representacion Visual y Main

--- torre/torre.cpp
+++ torre/torre.cpp
@@ -36,7 +36,7 @@ public:
 	{
 		if (f < 1 || f > 8 || c < 1 || c > 8)
 		{
-			cout << "Error: movimiento fuera de los limites del tablero (1-8)." << endl;
+			cout << "movimiento fuera de los limites del tablero (1-8)." << endl;
 			return;
 		}
 
@@ -48,12 +48,64 @@ public:
 		}
 		else
 		{
-			cout << "Error: movimiento invalido. La Torre solo se mueve en horizontal o vertical." << endl;
+			cout << "movimiento invalido. La Torre solo se mueve en horizontal o vertical." << endl;
 		}
+	}
+	string draw()
+	{
+		if(isblack == true)
+		{
+			return "[##]";
+		}
+		else
+		{
+			return "[TT]";
+		}
+	}
+	void printState()
+	{
+		string color;
+		if (isblack == true)
+		{
+			color = "Negro";
+		}
+		else
+		{
+			color = "Blanco";
+		}
+		cout<< draw()<< " - " << color << " en la fila " << fila << ", columna " << columna << endl;
 	}
 };
 
 int main()
 {
+	torre torreBlanca(1, 1, false);
+	torre torreNegra(8, 8, true);
+
+	cout << "inicio" << endl;
+	torreBlanca.printState();
+	torreNegra.printState();
+
+	int f, c;
+	cout << "Mover torre blanca" << endl;
+	cout << "Ingrese fila (1-8): ";
+	cin >> f;
+	cout << "Ingrese columna (1-8): " << endl;
+	cin >> c;
+	torreBlanca.movimiento(f, c);
+	torreBlanca.printState();
+
+	cout << "Mover torre Negra" << endl;
+	cout << "Ingrese fila (1-8): ";
+	cin >> f;
+	cout << "Ingrese columna (1-8): " << endl;
+	cin >> c;
+	torreNegra.movimiento(f, c);
+	torreNegra.printState();
+
+	cout << "Movimiento diagonal ilegal" << endl;
+	torreBlanca.movimiento(f + 2, c + 2);
+	torreBlanca.printState();
+
 	return 0;
 }
